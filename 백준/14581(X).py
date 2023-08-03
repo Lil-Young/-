9f62f659095c89ecaf737b56c8f8641c558d4a0f
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2023-08-03 18:37:36 +0900

    update

--- 백준/14581(X).py
+++
@@ -1,7 +0,0 @@
-id = input().rstrip()
-print(f'''
-:fan::fan::fan:
-:fan::{id}::fan:
-:fan::fan::fan:
-      ''')
-# https://www.acmicpc.net/problem/14581
---
+++ 백준/14581.py
@@ -0,0 +1,5 @@
+id = input()
+
+print(':fan::fan::fan:')
+print(':fan::%s::fan:' % id)
+print(':fan::fan::fan:')
