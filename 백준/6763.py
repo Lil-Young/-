185f40f0154b46daa6a324596e66cc76dc7de157
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2023-08-26 06:41:53 +0900

    update

--- 백준/6763.py
+++ 백준/6763.py
@@ -6,5 +6,3 @@ if b-a >= 31: print(f"You are speeding and your fine is $500.")
 elif b-a >= 21 and b-a <= 30: print(f"You are speeding and your fine is $270.")
 elif b-a >= 1 and b-a <= 20: print(f"You are speeding and your fine is $100.")
 else: print("Congratulations, you are within the speed limit!")
-
-# https://www.acmicpc.net/problem/6763
