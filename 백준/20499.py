378aa94062e231220bcb2849b7b3b713d78f4797
Lil-Young <thddyddls1@naver.com>
2023-10-29 22:21:02 +0900

    update

--- 백준/20499.py
+++ 백준/20499.py
@@ -4,5 +4,3 @@ word = list(input().rstrip().split('/'))
 k, d, a = int(word[0]), int(word[1]), int(word[2])
 if k+a < d or d == 0: print("hasu")
 else: print("gosu")
-
-# https://www.acmicpc.net/problem/20499
