7bc41192ccb901175ea22d7d14b0659e720baefd
Lil-Young <thddyddls1@naver.com>
2023-11-14 19:56:54 +0900

    update

--- 백준/24568.py
+++ 백준/24568.py
@@ -1,5 +1,3 @@
 first = int(input())
 second = int(input())
 print((first*8 + second*3)-28)
-
-# https://www.acmicpc.net/problem/24568
--- 백준/24723(X).py
+++
@@ -1,5 +0,0 @@
-import sys
-input = sys.stdin.readline
-print(2 ** int(input()))
-
-# https://www.acmicpc.net/problem/24723
