c0b3ce927f01b6ea5104d02d781ea08f15a47010
Lil-Young <thddyddls1@naver.com>
2024-02-22 03:31:21 +0900

    update

--- 백준/1463(못풀었음).py
+++ 백준/1463(못풀었음).py
@@ -1,57 +1,20 @@
-import sys
+def is_square(x):
+    if (x & (x - 1)):
+        return False
+    else:
+        return True
+
+import sys, math
 input = sys.stdin.readline
 X = int(input())
-fix_X = X
-count = 0
-num = 0
-count_list = []
-while True:
-    if num == 0:
-        count+=1
-        if X%3 == 0: X//=3
-        elif X%2 == 0: X//=2
-        else: X-=1
-        if X == 1: count_list.append(count); count = 0; num+=1
-    elif num == 1:
-        if count== 0: X = fix_X
-        count+=1
-        if X%3 == 0: X//=3
-        else: X-=1
-        if X%2 == 0: X//=2
-        if X == 1: count_list.append(count); count = 0; num+=1
-    elif num == 2:
-        if count== 0: X = fix_X
-        count+=1
-        if X%2 == 0: X//=2
-        elif X%3 == 0: X//=3
-        else: X-=1
-        if X == 1: count_list.append(count); count = 0; num+=1
-    elif num == 3:
-        if count== 0: X = fix_X
-        count+=1
-        if X%2 == 0: X//=2
-        else: X-=1
-        if X%3 == 0: X//=3
-        if X == 1: count_list.append(count); count = 0; num+=1
-    elif num == 4:
-        if count== 0: X = fix_X
-        count+=1
-        X-=1
-        if X%3 == 0: X//=3
-        elif X%2 == 0: X//=2
-        else: continue
-        if X == 1: count_list.append(count); count = 0; num+=1
-    elif num == 5:
-        if count== 0: X = fix_X
-        X = fix_X
-        count+=1
-        X-=1
-        if X%2 == 0: X//=2
-        elif X%3 == 0: X//=3
-        if X == 1: count_list.append(count); count = 0; num+=1
-    if num == 6: break    
-print(min(count_list))
-    
-    
+cnt = 0
+while X != 1:
+    if X % 2 == 0:
+        X //= 2
+        pass
+
+        
+
+
 
 # https://www.acmicpc.net/problem/1463
