c4f31f015c08c8031a76842f846ac3787e15033c
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-05-01 12:50:51 +0900

    update

--- 백준/1388.py
+++ 백준/1388.py
@@ -1,42 +1,33 @@
 def dfs1(x, y):
-    if x < -1 or x >= n or y < -1 or y >= m:
+    if x >= n or y >= m:
         return
-    if graph[x][y] == 'check':
-        return
-    if graph[x][y] == '|':
-        return
-    if graph[x][y] == '-':
-        graph[x][y] = 'check'
+    if visited[x][y] == 0 and graph[x][y] == '-':
+        visited[x][y] = 1
         dfs1(x, y+1)
         return True
     return False
 
 def dfs2(x, y):
-    if x < -1 or x >= n or y < -1 or y >= m:
+    if x >= n or y >= m:
         return
-    if graph[x][y] == 'check':
-        return
-    if graph[x][y] == '-':
-        return
-    if graph[x][y] == '|':
-        graph[x][y] = 'check'
+    if visited[x][y] == 0 and graph[x][y] == '|':
+        visited[x][y] = 1
         dfs2(x+1, y)
         return True
     return False
-
-
 
 n, m = map(int, input().split())
 graph = []
 for i in range(n):
     graph.append(list(input()))
 
+visited = [[0]*m for _ in range(n)]
+
 result = 0
 for i in range(n):
     for j in range(m):
-        if dfs1(i, j) == True:
-            result += 1
-        if dfs2(i, j) == True:
+        if dfs1(i, j) == True or dfs2(i, j) == True:
             result += 1
 print(result)
-
+print(graph)
+print(visited)
