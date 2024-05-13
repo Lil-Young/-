857003f89f1f14ec2d83858d2e0bd1e2e35c714b
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-05-13 23:33:30 +0900

    update

--- 백준/31575.py
+++ 백준/31575.py
@@ -7,18 +7,3 @@ visited = [[False] * N for _ in range(M)]
 visited[0][0] = True
 dx = [0, 1]
 dy = [1, 0]
-
-def dfs(x, y):
-
-    for i in range(2):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx >= 0 and nx < M and ny >= 0 and ny < N and not visited[nx][ny] and graph[nx][ny] == 1:
-            visited[nx][ny] = True
-            dfs(nx, ny)
-                
-dfs(0, 0)
-if visited[M-1][N-1]:
-    print("Yes")
-else:
-    print("No")
