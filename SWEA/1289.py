2823a2873a732c1043b8fea72baed2bb4250fbef
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-05-18 22:53:45 +0900

    update

---
+++ SWEA/1289.py
@@ -0,0 +1,12 @@
+# 원재의 메모리 복구하기
+
+T = int(input())
+for tc in range(1, T+1):
+    s = input()
+    result = 0
+    loc = '0'
+    for i in s:
+        if loc != i:
+            loc = i
+            result+=1
+    print(result)
